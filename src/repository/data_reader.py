3b353990967279bf1f21d47e135a74bb59ad05b3
Shahzad <shahzad.munir@xorlogics.com>
2025-02-05 11:30:24 +0100

    added the filname meta column to get the file while reading from the raw

--- src/repository/data_reader.py
+++ src/repository/data_reader.py
@@ -2,6 +2,8 @@ from src.domain.turbine_schema import TurbineRawPuddle
 from pyspark.sql import DataFrame
 from pyspark.sql.utils import AnalysisException, IllegalArgumentException
 from pyspark.sql import SparkSession
+from  pyspark.sql.functions import input_file_name
+
 
 import logging
 
@@ -45,7 +47,8 @@ class DataReader:
             return self.spark.read \
                 .option("header", "true") \
                 .schema(TurbineRawPuddle().get_schema()) \
-                .csv(file_path)
+                .csv(file_path) \
+                .withColumn("filename", input_file_name())
 
         except AnalysisException as e:
             error_msg = f"Failed to analyze CSV file at {file_path}: {str(e)}"
@@ -61,3 +64,4 @@ class DataReader:
             error_msg = f"Unexpected error while reading CSV file at {file_path}: {str(e)}"
             self.logger.error(error_msg)
             raise DataReaderException(error_msg)
+        
